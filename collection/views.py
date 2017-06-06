1202fbe2427432f4fd9194d38b017c3c563c14d6
Radhika Gulati <radhigulati@gmail.com>
2017-06-06 11:33:01 -0700

    added more urls

--- collection/views.py
+++ collection/views.py
@@ -3,4 +3,10 @@ from django.shortcuts import render
 def index(request):
     # urls.py will catch that someone wants the homepage and points to this
     # piece of code which will render the index.html template
-    return render(request, 'index.html')
+    # render combines a given template with a given context dictionary and
+    # returns a HTTPResponse object with that rendered text
+    # render(request, template_name)
+    number = 6
+    return render(request, 'index.html', {
+        'number': number,
+    })
